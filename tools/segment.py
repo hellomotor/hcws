428e3dfcc8d025456a352a8b41591f4da841ceeb
heqing <blizzard2000@gmail.com>
2019-10-15 14:14:28 +0800

    1. 添加host, port选项

--- tools/segment.py
+++ tools/segment.py
@@ -22,6 +22,8 @@ BERT_BASE_DIR = os.path.join(HOME, 'git_repo/tf_ner/bert_lstm_crf/chinese_L-12_H
 BERT_VOCAB = os.path.join(BERT_BASE_DIR, 'vocab.txt')
 
 FLAGS = flags.FLAGS
+flags.DEFINE_string('host', '192.168.199.120', '')
+flags.DEFINE_integer('port', 8500, '')
 flags.DEFINE_string('input_file', None, '')
 flags.DEFINE_string('label_dict_file', LABEL_DICT_FILE, '')
 flags.DEFINE_string('q2b_file', Q2B_DICT_FILE, '')
@@ -29,11 +31,12 @@ flags.DEFINE_string('model_name', None, '')
 flags.DEFINE_string('vocab_file', BERT_VOCAB, '')
 flags.DEFINE_integer('max_sequence_length', 200, '')
 flags.DEFINE_integer('batch_size', 10, '')
+flags.DEFINE_bool('ignore_space', False, 'ignore space chars in input_file.')
+flags.DEFINE_bool('output_pos', True, 'output POS tag.')
 
 
 def create_grpc_stub():
-    host, port = '192.168.199.120', 8500
-    channel = grpc.insecure_channel('{}:{}'.format(host, port))
+    channel = grpc.insecure_channel('{}:{}'.format(FLAGS.host, FLAGS.port))
     return prediction_service_pb2_grpc.PredictionServiceStub(channel)
 
 
@@ -51,7 +54,12 @@ def inference(stub, features):
 
 
 def predict(stub, tokenizer, q2b_dict, id_to_label, batch_of_text):
-    feature = create_feature_from_tokens(tokenizer, q2b_dict, batch_of_text, FLAGS.max_sequence_length)
+    feature = create_feature_from_tokens(
+        tokenizer, 
+        q2b_dict, 
+        batch_of_text, 
+        FLAGS.max_sequence_length,
+        ignore_space=FLAGS.ignore_space)
     result = inference(stub, feature)
     seq_lens = tensor_util.MakeNdarray(result.outputs['seq_lens'])
     pred_ids = tensor_util.MakeNdarray(result.outputs['predictions'])
@@ -63,7 +71,7 @@ def predict(stub, tokenizer, q2b_dict, id_to_label, batch_of_text):
         token_ids = input_ids[i][:end]
         labels = [id_to_label.get(_id, 'O') for _id in label_ids[1:-1]]
         tokens = tokenizer.convert_ids_to_tokens(token_ids[1:-1])
-        output = label_decode(tokens, labels)
+        output = label_decode(tokens, labels, output_pos=FLAGS.output_pos)
         print((u''.join(output)).encode('utf8'))
 
 
@@ -73,17 +81,17 @@ def main(_):
     q2b_dict = load_plain_dict(FLAGS.q2b_file)
 
     tokenizer = tokenization.FullTokenizer(vocab_file=FLAGS.vocab_file, do_lower_case=True)
-    stub = create_grpc_stub()
-    for batch_of_line in batch_read_iter(FLAGS.input_file, FLAGS.batch_size):
-        batch_of_tokens = [(w for w in line.strip('\n')[:FLAGS.max_sequence_length])
-                           for line in batch_of_line]
-        predict(stub, tokenizer, q2b_dict, id_to_label, batch_of_tokens)
+    try:
+        stub = create_grpc_stub()
+        for batch_of_line in batch_read_iter(FLAGS.input_file, FLAGS.batch_size):
+            batch_of_tokens = [(w for w in line.strip('\n')[:FLAGS.max_sequence_length])
+                               for line in batch_of_line]
+            predict(stub, tokenizer, q2b_dict, id_to_label, batch_of_tokens)
+    except KeyboardInterrupt:
+        print('User breaked .')
 
 
 if __name__ == '__main__':
     flags.mark_flag_as_required('input_file')
     flags.mark_flag_as_required('model_name')
-    try:
-        app.run(main)
-    except KeyboardInterrupt:
-        print('User breaked .')
+    app.run(main)
